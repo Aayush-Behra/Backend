9161569d2d6becbb9c39519a3b3af442b8b1e796
Ezie <73498899+Aayush-Behra@users.noreply.github.com>
2026-07-04 11:41:00 +0530

    Fix import path for app module in server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import app from "./src/src/app";
+import app from "./src/app.ts";
 import { env } from "./src/config/env";
 import { connectDatabase } from "./src/config/db";
 import { Persona, buildDefaultPersonas } from "./src/models/Persona";
